da50f344fe1cba63d136101ddb9534d893e0baa5
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-11-14 05:16:33 -0600

    camara

---
+++ 1114-modulos_camara/02-abrir_camera_espejo.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+if not camara.isOpened():
+    print("No puedo abrir la camara")
+    exit(1)
+
+while True:
+    # Leemos la imagen de l camara
+    ret, imagen = camara.read()
+
+    if not ret:
+        print("No podemos capturar la imagen de la camara")
+        break
+
+    imagen_flip = cv.flip(imagen, 0)
+    #  0 de Cabeza
+    #  1 Espejo
+    # -1 de Cabeza espejo
+
+    cv.imshow("Camara", imagen)
+    cv.imshow("Camara Flip", imagen_flip)
+
+    # Al oprimir ESC salimos del programa
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()
---
+++ 1114-modulos_camara/09_imagnes_por_canal_Lenna.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("lenna.jpg")
+
+# Separar los canales en RGB
+b,g,r = cv.split(img)
+zeros = np.zeros(b.shape, dtype=np.uint8)
+
+cv.imshow("Lenna", img)
+cv.imshow("Zeros", zeros)
+cv.imshow("Rojo", r)
+cv.imshow("Verde", g)
+cv.imshow("Azul", b)
+
+while True:
+    k = cv.waitKey(0)
+    if k == 27: # 27 es el escape (ESC)
+        break
+cv.destroyAllWindows()
